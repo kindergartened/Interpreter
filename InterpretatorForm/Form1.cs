d28c6a625dda6d1a9c8e2a9d7377a671d31b433b
Pavelavl <sfdrus2004@gmail.com>
2024-02-23 20:50:08 +0300

    some changes

--- InterpretatorForm/Form1.cs
+++ InterpretatorForm/Form1.cs
@@ -1,71 +1,68 @@
 using Intepreter;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 
-namespace InterpretatorForm
+namespace InterpretatorForm;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private string _expresion = "";
+    private readonly ExpressionInterpreter _expressionInterpreter = new();
+    public Form1()
     {
-        private string expresion = "";
-        private readonly ExpressionInterpreter expressionInterpreter = new();
-        public Form1()
-        {
-            InitializeComponent();
-            ButtonsOptions(false, "#4682b4");
-        }
+        InitializeComponent();
+        ButtonsOptions(false, "#4682b4");
+    }
 
-        private void button2_Click(object sender, EventArgs e) //save and calc
+    private void button2_Click(object sender, EventArgs e) //save and calc
+    {
+        try
         {
-            try
+            if (richTextBox1.Text == "")
             {
-                if (richTextBox1.Text == "")
-                {
-                    Error("Expression not entered");
-                }
-                else
-                {
-                    expresion = richTextBox1.Text;
-                    richTextBox3.Text = expressionInterpreter.Interpret(expresion).ToString();
-                    ButtonsOptions(true, "#0066CC");
-                }
+                Error("Expression not entered");
             }
-            catch (Exception)
+            else
             {
-                Error("Unknown error =(");
+                _expresion = richTextBox1.Text;
+                richTextBox3.Text = _expressionInterpreter.Interpret(_expresion).ToString();
+                ButtonsOptions(true, "#0066CC");
             }
         }
-
-        private void button1_Click(object sender, EventArgs e) //Reverse Polish Note
+        catch (Exception)
         {
-            richTextBox2.Text = expressionInterpreter.ConvertToPostfix(expresion);
+            Error("Unknown error =(");
         }
+    }
 
-        private void ButtonsOptions(bool enabled, string color)
-        {
-            button1.Enabled = enabled; button1.BackColor = ColorTranslator.FromHtml(color);
-            button3.Enabled = enabled; button3.BackColor = ColorTranslator.FromHtml(color);
-            button4.Enabled = enabled; button4.BackColor = ColorTranslator.FromHtml(color);
-        }
+    private void button1_Click(object sender, EventArgs e) //Reverse Polish Note
+    {
+        richTextBox2.Text = _expressionInterpreter.ConvertToPostfix(_expresion);
+    }
 
-        private static void Error(string text)
-        {
-            MessageBox.Show(
-                    text,
-                    "Error",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error,
-                    MessageBoxDefaultButton.Button1);
-        }
+    private void ButtonsOptions(bool enabled, string color)
+    {
+        button1.Enabled = enabled; button1.BackColor = ColorTranslator.FromHtml(color);
+        button3.Enabled = enabled; button3.BackColor = ColorTranslator.FromHtml(color);
+        button4.Enabled = enabled; button4.BackColor = ColorTranslator.FromHtml(color);
+    }
 
-        private void button3_Click(object sender, EventArgs e)
+    private static void Error(string text)
+    {
+        MessageBox.Show(
+            text,
+            "Error",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error,
+            MessageBoxDefaultButton.Button1);
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        List<Token> list = _expressionInterpreter.Tokenize(_expresion);
+        string result = "";
+        for (int i = 0; i < list.Count; i++)
         {
-            List<Token> list = expressionInterpreter.Tokenize(expresion);
-            string result = "";
-            for (int i = 0; i < list.Count; i++)
-            {
-                result += $"{list[i].Type}: {list[i].Value} \n";
-            }
-            richTextBox2.Text = result;
+            result += $"{list[i].Type}: {list[i].Value} \n";
         }
+        richTextBox2.Text = result;
     }
 }
